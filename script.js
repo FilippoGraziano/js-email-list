39a471bfe33e9e90c7dcaca75b1bf0e1a25aee2c
Filippo Graziano <filippograziano13@gmail.com>
2026-06-17 16:23:37 +0200

    add a button that load and reload the email

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
 const ulEmail = document.getElementById(`email-list`);
 const buttonReload = document.getElementById(`reload-email-list`)
 
-let html = ``;
-for (let i = 0; i < 10; i++) {
-
-    axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
-        .then(result => {
-
-            const email = result.data.response;
-            html += `<li>${email}</li>`;
-
-            ulEmail.innerHTML = html;
-        });
-}
-
 buttonReload.addEventListener(`click`, e => {
 
     let html = ``;
@@ -28,4 +15,6 @@ buttonReload.addEventListener(`click`, e => {
                 ulEmail.innerHTML = html;
             });
     }
+
+    if (buttonReload.innerText === `Carica email`) buttonReload.innerText = `Ricarica email`
 })
