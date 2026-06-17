fe7f637e965a4a800dbaec5077a610a0f988ae18
Filippo Graziano <filippograziano13@gmail.com>
2026-06-17 16:18:06 +0200

    implement a button that reload the email list

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-const ul = document.getElementById(`email-list`);
+const ulEmail = document.getElementById(`email-list`);
+const buttonReload = document.getElementById(`reload-email-list`)
 
+let html = ``;
 for (let i = 0; i < 10; i++) {
 
     axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
         .then(result => {
-            
-            const email = result.data.response;
-            let html = `<li>${email}</li>`;
 
-            ul.innerHTML += html
+            const email = result.data.response;
+            html += `<li>${email}</li>`;
+
+            ulEmail.innerHTML = html;
         });
 }
+
+buttonReload.addEventListener(`click`, e => {
+
+    let html = ``;
+    for (let i = 0; i < 10; i++) {
+
+        axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
+            .then(result => {
+
+                const email = result.data.response;
+                html += `<li>${email}</li>`;
+
+                ulEmail.innerHTML = html;
+            });
+    }
+})
